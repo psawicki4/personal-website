73528d7b551721bc0651e31e7c769909fe967ec5
Piotr Sawicki <psawicki4@gmail.com>
2020-03-14 19:27:25 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'psa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'personal-website';
+}
