5b07780f95faf1d4ee35de206efc97578836a226
psawicki <psawicki4@gmail.com>
2026-04-20 13:53:12 +0200

    imports fix

--- apps/personal-website/src/app/app.component.ts
+++ apps/personal-website/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { afterNextRender, Component, inject } from '@angular/core';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
-import { inject as injectAnalytics } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import { HeaderComponent, MenuComponent } from 'personal-website-components';
 import { LangService } from 'utils';
 
