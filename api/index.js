2c8c57505add6b25e29b36fcc26dabcfb574f2bb
psawicki <psawicki4@gmail.com>
2026-05-09 21:42:29 +0200

    index.js reverted

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-import { join } from 'path';
-
 export default async (req, res) => {
-  const projectRoot = process.cwd();
   const { reqHandler } = await import('../dist/apps/personal-website/server/server.mjs');
-  process.env['ASSETS_PATH'] = join(projectRoot, 'dist/apps/personal-website/browser');
   return reqHandler(req, res);
 };
